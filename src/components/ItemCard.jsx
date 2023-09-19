fb8069fdb6617eba83c4dd7fedf8b84f01ad94e1
pastaBoi07 <46807055+pastaBoi07@users.noreply.github.com>
2023-09-19 14:39:31 -0400

    Add basic styling for item cards

---
+++ src/components/ItemCard.jsx
@@ -0,0 +1,12 @@
+
+export default function ItemCard(){
+
+  return (
+    <div className="bg-black/80 h-80 w-64 m-6 flex justify-center items-center flex-col">
+      <div className="bg-white/40 h-48 w-48"></div>
+      <div className="text-zinc-100">Title</div>
+      <div className="text-white">$4</div>
+
+    </div>
+  )
+}
--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -1,10 +1,13 @@
+import ItemCard from "../components/ItemCard";
 
 
 export default function Catalog(){
 
   return(
-    <div className="bg-white/50 h-5/6 w-4/5 mt-6 self-center ">
-      
+    <div className="bg-white/30 h-5/6 w-4/5 mt-6 self-center flex flex-wrap justify-center overflow-y-scroll">
+      <ItemCard/>
+      <ItemCard/>
+      <ItemCard/>
     </div>
   )
 }
