9f8bd874b826810aaaf8e746cdb47fdebbb130b1
pastaBoi07 <46807055+pastaBoi07@users.noreply.github.com>
2023-09-20 11:36:10 -0400

    Add submission form to item cards

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -6,7 +6,15 @@ export default function ItemCard(){
       <div className="bg-white/40 h-48 w-48"></div>
       <div className="text-zinc-100">Title</div>
       <div className="text-white">$4</div>
-
+      <div className="flex">
+        <form action="">
+          <input className="w-14 text-center" 
+            name="numOfItems" type="number" defaultValue={0} min="0" max="20">
+          </input>
+          <input type="hidden" name="itemid" value="temp1234"></input>
+          <button className=" bg-red-400 w-20">Add</button>
+        </form>
+      </div>
     </div>
   )
 }
