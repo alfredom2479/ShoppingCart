f1d0fde926c501e83f3f8e7025c56cf3c0032987
pastaBoi07 <46807055+pastaBoi07@users.noreply.github.com>
2023-09-22 14:24:39 -0400

    Calculate and display cart total price

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -23,6 +23,15 @@ export async function loader(){
 export default function CartPage(){
 
   const cartItemsData = useLoaderData();
+  const subtotal = cartItemsData.reduce((acc, item)=> acc + item.totalPrice, 0);
+  const roundedSubtotal = Math.round(subtotal * 100) /100;
+  roundedSubtotal.toFixed(2);
+
+  const tax = Math.round(((subtotal * .11)+ Number.EPSILON) *  100) / 100;
+  tax.toFixed(2)
+
+  const finalPrice = Math.round((subtotal + tax) * 100) /100;
+  finalPrice.toFixed(2);
 
   return(
     <>
@@ -34,15 +43,15 @@ export default function CartPage(){
       <div className="bg-black/50 my-4 w-[80%] self-center flex items-center justify-around">
         <div className="text-white flex flex-col items-center">
           <p className="font-medium text-2xl">Subtotal</p>
-          <p className="text-green-500">$subprice</p>
+          <p className="text-green-500">${roundedSubtotal}</p>
         </div>
         <div className="text-white flex flex-col items-center">
           <p className="font-medium text-2xl">Tax & Fees</p>
-          <p className="text-green-500">$t&fprice</p>
+          <p className="text-green-500">${tax}</p>
         </div>
         <div className="text-white flex flex-col items-center">
           <p className="font-medium text-2xl">Total</p>
-          <p className="text-green-500">$totalprice</p>
+          <p className="text-green-500">${finalPrice}</p>
         </div>
         <button type="submit" form="cart" onClick={()=>localStorage.clear()}
         className="bg-green-400/20 my-6 w-48 h-16 self-center text-3xl font-medium rounded-full border-black border-4 hover:bg-green-400/70">
