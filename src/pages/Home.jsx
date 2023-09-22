57c0d96ab2283289f7934f1d52aa58164a15e066
pastaBoi07 <46807055+pastaBoi07@users.noreply.github.com>
2023-09-22 15:06:00 -0400

    Fix home navigation button

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-
+import { NavLink } from "react-router-dom";
 
 export default function Home(){
 
@@ -7,9 +7,9 @@ export default function Home(){
 
         <div className="w-96 h-52 bg-black/40 text-white flex items-center justify-center text-5xl flex-col font-black rounded-lg">
           Fake Store
-          <button className="bg-white/20 mt-8 text-orange-300 text-2xl    border-solid border-orange-300 w-32 border-4 rounded-lg hover:bg-white ">
+          <NavLink to="/shop" className="bg-white/20 mt-8 text-orange-300 text-center text-2xl border-solid border-orange-300 w-32 border-4 rounded-lg hover:bg-white ">
             Shop
-          </button>
+          </NavLink>
         </div>
      </div>
   );
